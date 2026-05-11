bdc440a4e6a6e7dfc04e4388af6da7ae66b6e869
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2026-05-11 10:21:00 +0700

    first commit

---
+++ src/test/java/com/BookingHomeStay/BookingHomeStay/BookingHomeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookingHomeStay.BookingHomeStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHomeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
